0018851e716cc4b95eb3aadd7fa564b7cccfaaa0
patrZZZ <45408726+patrZZZ@users.noreply.github.com>
2018-11-28 03:56:10 -0500

    Update Cpp_test.cpp

--- Cpp_test.cpp
+++ Cpp_test.cpp
@@ -1,6 +1,7 @@
 
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -88,6 +89,110 @@ long repeatedString(string s, long n) {
     return num;
 }
 
-   
+ while(condition) {
+   statement(s);
+}
 
-      
+if(boolean_expression 1) {
+   // Executes when the boolean expression 1 is true
+} else if( boolean_expression 2) {
+   // Executes when the boolean expression 2 is true
+} else if( boolean_expression 3) {
+   // Executes when the boolean expression 3 is true
+} else {
+   // executes when the none of the above condition is true.
+}
+
+   
+   char grade = 'D';
+
+   switch(grade) {
+      case 'A' :
+         cout << "Excellent!" << endl; 
+         break;
+      case 'B' :
+      case 'C' :
+         cout << "Well done" << endl;
+         break;
+      case 'D' :
+         cout << "You passed" << endl;
+         break;
+      case 'F' :
+         cout << "Better try again" << endl;
+         break;
+      default :
+         cout << "Invalid grade" << endl;
+   }
+   cout << "Your grade is " << grade << endl;
+
+//Array:
+int a[3][4] = {  
+   {0, 1, 2, 3} ,   
+   {4, 5, 6, 7} ,   
+   {8, 9, 10, 11}   
+};
+int a[3][4] = {0,1,2,3,4,5,6,7,8,9,10,11};
+   
+double *p;
+double balance[10];
+
+p = balance;
+   
+*(p+1)
+   
+char greeting[] = "Hello";
+  
+
+string str1 = "Hello";
+string str2 = "World";
+
+str3 = str1 + str2;
+len = str3.size();
+
+   
+struct Books {
+   char  title[50];
+   char  author[50];
+   char  subject[100];
+   int   book_id;
+} 
+void printBook( struct Books book )
+   
+struct Books *struct_pointer;
+struct_pointer = &Book1;
+struct_pointer->title;
+   
+class Box {
+   public:
+      double length;   // Length of a box
+      double breadth;  // Breadth of a box
+      double height;   // Height of a box
+};
+   
+double Box::getVolume(void) {
+   return length * breadth * height;
+}
+   
+Box::Box(double length) {
+   cout << "Object is being created" << endl;
+}
+Box::~Box(void) {
+   cout << "Object is being deleted" << endl;
+}
+//copy constructor
+Line::Line(const Line &obj) {
+   cout << "Copy constructor allocating ptr." << endl;
+   ptr = new int;
+   *ptr = *obj.ptr; // copy the value
+}
+//Derived class
+class Rectangle: public Shape {
+   public:
+   a = 1；
+      ｝
+class Rectangle: public Shape, public PaintCost { 
+
+
+
+   
+   
