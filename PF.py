2f036d60d9cde0c4a98c62e2e05f4a1c5c1483fc
patrZZZ <45408726+patrZZZ@users.noreply.github.com>
2019-01-18 02:26:28 -0500

    Update PF.py

--- PF.py
+++ PF.py
@@ -1,1 +1,15 @@
+#Efficient Data and Financial Analytics with Python  Hilpisch Yves
+
+
+
+
+
 np.where(df['col1-col2']>C, 1, 0)
+np.random.standard_normal($size)
+
+#numexpr
+import numexpr as ne
+ne.set_num_threads(4)
+f = '3*log(a)+cos(a) ** 2'
+%timeit r = ne.evaluate(f)
+
